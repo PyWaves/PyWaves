a14c7b5255e830bf1291d37a388c4e00ec94258a
Yuri Dyachenko <pub.yurial@gmail.com>
2019-02-22 17:57:06 +0300

    more standard assets

--- __init__.py
+++ __init__.py
@@ -179,7 +179,14 @@ def validateAddress(address):
 
 WAVES = Asset('')
 BTC = Asset('8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS')
-USD = Asset('Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck')
+BCH = Asset('zMFqXuoyrn5w17PFurTqxB7GsS71fp9dfk6XFwxbPCy')
+BSV = Asset('62LyMjcr2DtiyF5yVXFhoQ2q414VPPJXjsNYp72SuDCH')
 LTC = Asset('HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk')
-
+ETH = Asset('474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu')
+XMR = Asset('5WvPKSJXzVE2orvbkJ8wsQmmQKqTv9sGBPksV4adViw3')
+DASH = Asset('B3uGHFRpSUuGEDWjqB9LWWxafQj8VTvpMucEyoxzws5H')
+TRY = Asset('2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN')
+CNY = Asset('DEJbZipbKQjwEiRjx2AqQFucrj5CZ3rAc4ZvFM8nAsoA')
+EUR = Asset('Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU')
+USD = Asset('Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck')
 
