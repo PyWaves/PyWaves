1fe4e2d58f34c60d36ffdfb633cba81b6227e166
bnagtega <bram.nagtegals@trendminer.com>
2019-11-17 15:44:04 +0100

    make WAVES a variable

--- order.py
+++ order.py
@@ -19,7 +19,7 @@ class Order(object):
 
     def status(self):
         try:
-            req = pywaves.wrapper('/matcher/orderbook/%s/%s/%s' % ('WAVES' if self.assetPair.asset1.assetId=='' else self.assetPair.asset1.assetId, 'WAVES' if self.assetPair.asset2.assetId=='' else self.assetPair.asset2.assetId, self.orderId), host=self.matcher)
+            req = pywaves.wrapper('/matcher/orderbook/%s/%s/%s' % (DEFAULT_CURRENCY if self.assetPair.asset1.assetId=='' else self.assetPair.asset1.assetId, DEFAULT_CURRENCY if self.assetPair.asset2.assetId=='' else self.assetPair.asset2.assetId, self.orderId), host=self.matcher)
             return req['status']
         except:
             pass
